e83ab1a2109a2fb98018bec54ef8edbb439e857b
zakria <zakria786@gmail.com>
2020-09-28 12:10:34 +0500

    login page

---
+++ app/src/main/java/com/example/registration_form/Forget_password.java
@@ -0,0 +1,20 @@
+package com.example.registration_form;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Forget_password extends AppCompatActivity {
+
+    Button btn_cancel;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_forget_password);
+
+    }
+}
